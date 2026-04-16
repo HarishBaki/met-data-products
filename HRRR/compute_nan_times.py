c0faee9806415599eb401f7009e8f0a8356a09bf
HarishBaki <harish.iiitn@gmail.com>
2026-04-16 17:16:57 -0400

    Nan times is being computed.

---
+++ HRRR/compute_nan_times.py
@@ -0,0 +1,18 @@
+# %%
+import xarray as xr
+import zarr
+import os
+zarr_store = '/network/rit/lab/basulab/Projects/DFS/DATA/HRRR_NYS/HRRR_NYS.zarr'
+output_dir = '/network/rit/lab/basulab/Projects/DFS/DATA/HRRR_NYS/nan_times'
+os.makedirs(output_dir, exist_ok=True)
+ds = xr.open_zarr(zarr_store)
+# %%
+var_names = list(ds.data_vars)
+for var_name in var_names:
+    print(var_name)
+    nan_mask = ds[var_name].isnull().any(dim=("y", "x"))
+
+    nan_times = ds["time"].where(nan_mask).sel(time=slice("2014-01-01", "2025-12-31")).dropna("time").compute()
+
+    nan_times.to_netcdf(f"{output_dir}/nan_times_{var_name}.nc")
+    print(f"[done] Missing timestamps saved → {output_dir}/nan_times_{var_name}.nc")
